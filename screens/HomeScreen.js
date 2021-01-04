63e06a2460257ab6def55dc0fcca81769c297cd4
Chris Chapin <cchapin@outlook.com>
2021-01-03 23:05:43 -0800

    test for gpg prompt

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -14,6 +14,7 @@ import {} from '../constants/Service';
 //
 
 // remove class and use hooks
+// test for commit prompt
 export default class HomeScreen extends React.Component {
   static propTypes = {
     navigation: PropTypes.shape({
