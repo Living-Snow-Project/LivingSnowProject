88cbe09017ce7365c331de5016d395a559601857
Chris Chapin <cchapin@outlook.com>
2021-05-24 09:10:12 -0700

    improve PhotoControl visual experience

--- components/PhotoControl.js
+++ components/PhotoControl.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image, Pressable, View } from 'react-native';
+import { Image, Pressable, StyleSheet, View } from 'react-native';
 import PropTypes from 'prop-types';
 import { PictureIcon } from '../components/TabBarIcon';
 import { Routes } from '../navigation/Routes';
@@ -9,10 +9,10 @@ const PhotoControl = ({navigation, photos}) => {
     <Pressable onPress={() => navigation.navigate(Routes.ImagesPickerScreen)}>
       {photos.length === 0 && <PictureIcon/>}
       {photos.length > 0 && 
-      <View style={{flex:photos.length, flexDirection: "row"}}>{
+      <View style={{flex:photos.length, flexDirection: 'row'}}>{
         photos.map((x, index) =>
-        <View style={{flex:1, margin:3}} key={index}>
-          <Image style={{width: "100%", height: 100}} key={index} source={{uri: x.uri}}/>
+        <View style={styles.singlePhotoContainer} key={index}>
+          <Image style={styles.photo} key={index} source={{uri: x.uri}}/>
         </View>)}
       </View>}
     </Pressable>
@@ -25,5 +25,21 @@ PhotoControl.propTypes = {
   }),
   photos: PropTypes.array,
 };
+
+const styles = StyleSheet.create({
+  // these styles could be dynamically sized\arranged based on number of photos and combinaions of portrait\landscape
+  singlePhotoContainer: {
+    flex:1,
+    margin:1,
+    height: 100
+  },
+  photo: {
+    width: '100%',
+    height: '100%',
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 2
+  }
+});
 
 export { PhotoControl };
