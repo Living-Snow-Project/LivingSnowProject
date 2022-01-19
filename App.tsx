2ec9e2addd6d47bcf949543276e2082b699df0de
Chris Chapin <cchapin@outlook.com>
2022-01-19 12:18:39 -0800

    app.tsx and jest fallout

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/jsx-filename-extension */
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
