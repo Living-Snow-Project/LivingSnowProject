13c2514e16bb03159c36a0538c8085b190cc3af2
Chris Chapin <cchapin@outlook.com>
2022-01-23 09:45:21 -0800

    cleaner jest config

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+
+module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "**/*.{js,jsx,ts,tsx}",
+    "!**/coverage/**",
+    "!**/expo-images-picker/**",
+    "!**/node_modules/**",
+    "!**/babel.config.js",
+    "!**/jesttest.setup.js",
+    "!**/jest.config.js",
+  ],
+  moduleFileExtensions: ["ts", "tsx", "js"],
+  preset: "jest-expo",
+  setupFiles: ["./jesttest.setup.js"],
+  setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
+  transform: {
+    "\\.[jt]sx?$": "babel-jest",
+  },
+};
