681c002b007df12f938aed104d9f469556d04151
Chris Chapin <cchapin@outlook.com>
2022-01-31 20:41:32 -0800

    seed TimelineScreen testing

---
+++ src/screens/__tests__/TimelineScreen-test.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { render } from "@testing-library/react-native";
+import TimelineScreen from "../TimelineScreen";
+
+const navigation = {
+  navigate: jest.fn(),
+  addListener: () => () => {},
+};
+
+describe("TimelineScreen test suite", () => {
+  test("renders", () => {
+    const { toJSON } = render(<TimelineScreen navigation={navigation} />);
+    expect(toJSON()).toMatchSnapshot();
+  });
+
+  test.todo("download records");
+  test.todo("saved records");
+  test.todo("offline mode");
+  test.todo("pull to refresh");
+  test.todo("scrolling");
+  test.todo("navigate to settings screen");
+  test.todo("navigate to record screen");
+  test.todo("navigate to record details screen");
+});
