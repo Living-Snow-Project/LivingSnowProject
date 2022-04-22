6ed7dff862236da8d8b5301531f215424ad1fce4
Chris Chapin <cchapin@outlook.com>
2022-04-22 15:13:53 -0700

    status bar animation

--- src/components/StatusBar.tsx
+++ src/components/StatusBar.tsx
@@ -1,13 +1,9 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useRef } from "react";
+import { Animated, StyleSheet, Text } from "react-native";
 import PropTypes from "prop-types";
 import { Labels } from "../constants/Strings";
 
 const styles = StyleSheet.create({
-  container: {
-    borderBottomWidth: 1,
-    backgroundColor: "lightgrey",
-  },
   text: {
     fontSize: 16,
     textAlign: "center",
@@ -20,12 +16,51 @@ type StatusBarProps = {
 };
 
 export default function StatusBar({ state, isConnected }: StatusBarProps) {
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+  const isAnimating = useRef(false);
+  const currentState = useRef(state);
+  const color = fadeAnim.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["rgb(211, 211, 211)", "rgb(155, 155, 155)"],
+  });
+
+  currentState.current = state;
+
+  const animate = () => {
+    isAnimating.current = true;
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: false,
+    }).start(() => {
+      Animated.timing(fadeAnim, {
+        toValue: 0,
+        duration: 1000,
+        useNativeDriver: false,
+      }).start(() => {
+        isAnimating.current = false;
+        if (currentState.current !== "Idle") {
+          animate();
+        }
+      });
+    });
+  };
+
+  if (state !== "Idle" && !isAnimating.current) {
+    animate();
+  }
+
   return (
-    <View style={styles.container}>
+    <Animated.View
+      style={{
+        borderBottomWidth: 1,
+        backgroundColor: color,
+      }}
+    >
       <Text style={styles.text}>
         {isConnected ? state : Labels.StatusBar.NoConnection}
       </Text>
-    </View>
+    </Animated.View>
   );
 }
 
