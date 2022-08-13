38b0da962b49e303e210ee6ced17057d4d067ed6
Thomas Cowan <thomas.cowan@gmail.com>
2022-08-13 14:41:30 -0700

    fix: add reanimated to babel config
    this solves the reanimated errors that crash expo go during testing, see the follwing github discussion: https://github.com/software-mansion/react-native-reanimated/issues/3364

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,6 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo', '@babel/preset-typescript'],
+    plugins: ["react-native-reanimated/plugin"]
   };
 };
