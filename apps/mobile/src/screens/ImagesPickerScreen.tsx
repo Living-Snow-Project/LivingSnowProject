a8c74b7c79e6a0c1de0133fe0e226a5fea03e9ed
Chris Chapin <cchapin@outlook.com>
2022-11-20 15:59:56 -0800

    fix: use new expo-images-picker API

--- apps/mobile/src/screens/ImagesPickerScreen.tsx
+++ apps/mobile/src/screens/ImagesPickerScreen.tsx
@@ -1,64 +1,87 @@
-import React, { useCallback } from "react";
-import { Platform, Text } from "react-native";
+import React from "react";
+import { Platform } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { ImagesPickerScreenProps } from "../navigation/Routes";
 /* eslint-disable import/no-relative-packages */
 import { AssetsSelector } from "../../expo-images-picker";
+import { SettingsType, StylesType } from "../../expo-images-picker/src/Types";
 
 export default function ImagesPickerScreen({
   navigation,
   route,
 }: ImagesPickerScreenProps) {
-  const noAssets = useCallback(() => <Text>No Assets</Text>, []);
+  const widgetSettings: SettingsType = {
+    getImageMetaData: false,
+    initialLoad: 100,
+    assetsType: ["photo"],
+    minSelection: 0,
+    maxSelection: 4,
+    portraitCols: 4,
+    landscapeCols: 5,
+  };
+
+  const widgetStyles: StylesType = {
+    margin: 3,
+    bgColor: "white",
+    spinnerColor: "blue",
+    widgetWidth: 100,
+    screenStyle: {
+      borderRadius: 5,
+      overflow: "hidden",
+    },
+    widgetStyle: {
+      margin: 10,
+    },
+    videoIcon: {
+      Component: Ionicons,
+      iconName: Platform.OS === "ios" ? "ios-videocam" : "md-videocam",
+      color: "white",
+      size: 20,
+    },
+    selectedIcon: {
+      Component: Ionicons,
+      iconName:
+        Platform.OS === "ios"
+          ? "ios-checkmark-circle-outline"
+          : "md-checkmark-circle-outline",
+      color: "grey",
+      bg: "lightgrey",
+      size: 40,
+    },
+  };
+
+  const widgetNavigator = {
+    Texts: {
+      finish: "Finish",
+      back: "Back",
+      selected: "",
+    },
+    buttonTextStyle: {},
+    buttonStyle: {},
+    // TODO: this should be on react-navigation header not the component's
+    // TODO: possible to eliminate goBackText and backFunction?
+    onBack: () => () => {},
+    onSuccess: (data: any[]) => {
+      route.params.onUpdatePhotos(data);
+      navigation.goBack();
+    },
+  };
+
+  const widgetResize = {
+    compress: 0.4,
+    width: 1024, // TODO: ideally this would be the size constraint on the largest axis
+    base64: false,
+    saveTo: "jpeg",
+  };
+
   return (
+    // TODO: how to tell what's already selected
     <AssetsSelector
-      options={{
-        manipulate: {
-          compress: 0.4,
-          width: 1024, // TODO: ideally this would be the size constraint on the largest axis
-          base64: false,
-          saveTo: "jpeg",
-        },
-        assetsType: ["photo"],
-        maxSelections: 4,
-        margin: 3,
-        portraitCols: 4,
-        landscapeCols: 5,
-        widgetWidth: 100,
-        widgetBgColor: "white",
-        spinnerColor: "blue",
-        videoIcon: {
-          Component: Ionicons,
-          iconName: Platform.OS === "ios" ? "ios-videocam" : "md-videocam",
-          color: "white",
-          size: 20,
-        },
-        selectedIcon: {
-          Component: Ionicons,
-          iconName:
-            Platform.OS === "ios"
-              ? "ios-checkmark-circle-outline"
-              : "md-checkmark-circle-outline",
-          color: "grey",
-          bg: "lightgrey",
-          size: 40,
-        },
-        defaultTopNavigator: {
-          // TODO: this should be on react-navigation header not the component's
-          // TODO: possible to eliminate goBackText and backFunction?
-          continueText: "Finish",
-          goBackText: ``,
-          buttonStyle: {},
-          buttonTextStyle: {},
-          selectedText: ``,
-          backFunction: () => {},
-          doneFunction: (data) => {
-            route.params.onUpdatePhotos(data);
-            navigation.goBack();
-          },
-        },
-        noAssets: () => noAssets(),
-      }}
+      Settings={widgetSettings}
+      Styles={widgetStyles}
+      Errors={{}}
+      Navigator={widgetNavigator}
+      Resize={widgetResize}
     />
   );
 }
