1dafe63a58757acb372135fd4a5151c83620b30a
Chris Chapin <cchapin@outlook.com>
2022-11-24 19:47:12 -0800

    perf: memoize calling expo-images-picker

--- apps/mobile/src/screens/ImagesPickerScreen.tsx
+++ apps/mobile/src/screens/ImagesPickerScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useMemo } from "react";
 import { Platform } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { Asset } from "expo-media-library";
@@ -42,74 +42,83 @@ function CustomNavigator({ navigation, onSuccess }: CustomNavigatorProps) {
   return null;
 }
 
+const widgetStyles: StylesType = {
+  margin: 3,
+  bgColor: "white",
+  spinnerColor: "blue",
+  widgetWidth: 100,
+  screenStyle: {
+    borderRadius: 5,
+    overflow: "hidden",
+  },
+  widgetStyle: {
+    margin: 10,
+  },
+  videoIcon: {
+    Component: Ionicons,
+    iconName: Platform.OS === "ios" ? "ios-videocam" : "md-videocam",
+    color: "white",
+    size: 20,
+  },
+  selectedIcon: {
+    Component: Ionicons,
+    iconName:
+      Platform.OS === "ios"
+        ? "ios-checkmark-circle-outline"
+        : "md-checkmark-circle-outline",
+    color: "red",
+    bg: "#00000000",
+    size: 40,
+  },
+};
+
+const widgetResize: ResizeType = {
+  compress: 0.7,
+  majorAxis: 1024,
+  base64: false,
+  saveTo: "jpeg",
+};
+
+// TODO: probably should do something here
+const widgetErrors = {};
+
 export default function ImagesPickerScreen({
   navigation,
   route,
 }: ImagesPickerScreenProps) {
-  const widgetSettings: SettingsType = {
-    getImageMetaData: false,
-    initialLoad: 100,
-    assetsType: ["photo"],
-    minSelection: 0,
-    maxSelection: 4,
-    existingSelectionIds: route.params.existingSelection,
-    portraitCols: 4,
-    landscapeCols: 5,
-  };
+  const widgetSettings: SettingsType = useMemo(
+    () => ({
+      getImageMetaData: false,
+      initialLoad: 100,
+      assetsType: ["photo"],
+      minSelection: 0,
+      maxSelection: 4,
+      existingSelectionIds: route.params.existingSelection,
+      portraitCols: 4,
+      landscapeCols: 5,
+    }),
+    [route]
+  );
 
-  const widgetStyles: StylesType = {
-    margin: 3,
-    bgColor: "white",
-    spinnerColor: "blue",
-    widgetWidth: 100,
-    screenStyle: {
-      borderRadius: 5,
-      overflow: "hidden",
-    },
-    widgetStyle: {
-      margin: 10,
-    },
-    videoIcon: {
-      Component: Ionicons,
-      iconName: Platform.OS === "ios" ? "ios-videocam" : "md-videocam",
-      color: "white",
-      size: 20,
-    },
-    selectedIcon: {
-      Component: Ionicons,
-      iconName:
-        Platform.OS === "ios"
-          ? "ios-checkmark-circle-outline"
-          : "md-checkmark-circle-outline",
-      color: "red",
-      bg: "#00000000",
-      size: 40,
-    },
-  };
-
-  const widgetResize: ResizeType = {
-    compress: 0.7,
-    majorAxis: 1024,
-    base64: false,
-    saveTo: "jpeg",
-  };
-
-  const customNavigatorWidget: CustomNavigatorType = {
-    Component: CustomNavigator,
-    props: {
-      navigation,
-      onSuccess: (data: Asset[]) => {
-        route.params.onUpdatePhotos(data);
-        navigation.goBack();
+  const customNavigatorWidget: CustomNavigatorType = useMemo(
+    () => ({
+      Component: CustomNavigator,
+      props: {
+        navigation,
+        onSuccess: (data: Asset[]) => {
+          route.params.onUpdatePhotos(data);
+          navigation.goBack();
+        },
       },
-    },
-  };
+    }),
+    [navigation, route]
+  );
 
   return (
     <AssetsSelector
       Settings={widgetSettings}
       Styles={widgetStyles}
-      Errors={{}}
+      Errors={widgetErrors}
       Resize={widgetResize}
       CustomNavigator={customNavigatorWidget}
     />
