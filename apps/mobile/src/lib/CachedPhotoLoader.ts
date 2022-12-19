20f723a8386f283b883bdb80288208d8e6c467b4
Chris Chapin <cchapin@outlook.com>
2022-12-18 20:12:02 -0800

    fix: save resized photo to disk manipulateAsync is expensive and results in UI locks

--- apps/mobile/src/lib/CachedPhotoLoader.ts
+++ apps/mobile/src/lib/CachedPhotoLoader.ts
@@ -1,7 +1,9 @@
 import {
+  copyAsync,
   documentDirectory,
   downloadAsync,
   getInfoAsync,
+  readAsStringAsync,
 } from "expo-file-system";
 import { manipulateAsync } from "expo-image-manipulator";
 import NetInfo from "@react-native-community/netinfo";
@@ -27,17 +29,20 @@ export async function getCachedPhoto({
   width,
   height,
 }: PhotoToCache): Promise<CachedPhotoResult> {
-  const resizeAndCache = async (
-    localFileUri: string,
-    fileCacheKey: string
-  ): Promise<CachedPhotoResult> => {
+  const localFileUri = `${documentDirectory}/${uri}.jpg`;
+  const fileCacheKey = `${uri}_${width}_${height}`;
+  const resizedLocalFileUri = `${documentDirectory}/${fileCacheKey}.jpg`;
+
+  const resizeAndCache = async (): Promise<CachedPhotoResult> => {
+    // this call is very slow and results in UI locks
     const resized = await manipulateAsync(
       localFileUri,
       [{ resize: { width, height } }],
       { base64: true }
     );
 
-    // TODO: save resized.uri to disk (currently in cacheDirectory and would need to be written to documentDirectory)
+    // save resized photo to disk
+    await copyAsync({ from: resized.uri, to: resizedLocalFileUri });
 
     const base64uri = `data:image/jpg;base64,${resized.base64}`;
     cachedPhotos.set(fileCacheKey, base64uri);
@@ -45,27 +50,19 @@ export async function getCachedPhoto({
     return { uri: base64uri, state: "Loaded" };
   };
 
-  // no action if static photo from require(...)
-  if (typeof uri == "number") {
-    return { uri, state: "Loaded" };
-  }
-
-  // photo already exists on disk (pending photos scenario)
-  if (uri.includes("file:///")) {
+  // no action if static photo from require(...) or photo already on disk (pending photos scenario)
+  if (typeof uri == "number" || uri.includes("file:///")) {
     return { uri, state: "Loaded" };
   }
 
   const remoteFileUri = downloadPhotoUri(uri);
 
-  // can't save photo to hard drive
+  // can't save photo to disk, force download
   if (documentDirectory == null) {
     return { uri: remoteFileUri, state: "Loaded" };
   }
 
-  const localFileUri = `${documentDirectory}/${uri}.jpg`;
-  const fileCacheKey = `${uri}_${width}_${height}`;
-
-  // is in memory cache?
+  // photo in memory cache?
   const cachedBase64 = cachedPhotos.get(fileCacheKey);
 
   if (cachedBase64) {
@@ -75,19 +72,34 @@ export async function getCachedPhoto({
     };
   }
 
-  // TODO: is resized file on disk
-  // then use readAsStringAsync() instead of doing manipulateAsync() again
+  // resized photo on disk?
+  const { exists } = await getInfoAsync(resizedLocalFileUri);
 
-  const { exists } = await getInfoAsync(localFileUri);
-  // photo already written to disk (but not in memory cache)
+  // resized photo already written to disk (but not in memory cache)
   if (exists) {
-    return resizeAndCache(localFileUri, fileCacheKey);
+    const diskBase64 = await readAsStringAsync(resizedLocalFileUri, {
+      encoding: "base64",
+    });
+
+    const base64uri = `data:image/jpg;base64,${diskBase64}`;
+
+    cachedPhotos.set(fileCacheKey, base64uri);
+
+    return { uri: base64uri, state: "Loaded" };
+  }
+
+  const localExists = await getInfoAsync(localFileUri);
+
+  // original photo already written to disk (but not in memory cache)
+  if (localExists.exists) {
+    return resizeAndCache();
   }
 
   const { isConnected } = await NetInfo.fetch();
 
   // photo doesn't exist locally; it must be downloaded
-  // however, downloadAsync spins infinitely if app is offline (in Expo 44), so check Network status
+  // however, downloadAsync spins infinitely if app is offline (in Expo 44)
+  // check Network status before initiating download
   if (!isConnected) {
     return { uri: remoteFileUri, state: "Offline" };
   }
@@ -95,8 +107,9 @@ export async function getCachedPhoto({
   const { status } = await downloadAsync(remoteFileUri, localFileUri);
 
   if (status == 200) {
-    return resizeAndCache(localFileUri, fileCacheKey);
+    return resizeAndCache();
   }
 
+  // download failed, photo unresolved
   return { uri: remoteFileUri, state: "Error" };
 }
