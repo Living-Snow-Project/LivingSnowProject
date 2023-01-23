32bdbc732867d7aba3305e1aa08ba5d017788ee7
Chris Chapin <cchapin@outlook.com>
2023-01-22 22:39:43 -0800

    test: don't use random numbers in snapshots

--- apps/mobile/src/components/test/TimelineRow.test.tsx
+++ apps/mobile/src/components/test/TimelineRow.test.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { render } from "@testing-library/react-native";
-import { makeExampleRecord } from "@livingsnow/record";
+import {
+  AlgaeRecord,
+  makeExampleRecord,
+  makeExamplePhoto,
+} from "@livingsnow/record";
 import { NativeBaseProviderForTesting } from "../../../jesttest.setup";
 import { TimelineRow } from "../screens";
 import { Labels } from "../../constants";
 
 describe("TimelineRow test suite", () => {
-  let expectedRecord;
+  let expectedRecord: AlgaeRecord;
 
   beforeEach(() => {
     expectedRecord = makeExampleRecord("Sample");
+    expectedRecord.photos = [makeExamplePhoto({ width: 720, height: 480 })];
   });
 
   test("renders", () => {
