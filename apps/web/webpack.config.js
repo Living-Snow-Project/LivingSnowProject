b17de36c49516ce4d84b958fde22d2926ba2c477
Chris Chapin <cchapin@outlook.com>
2024-07-29 16:18:47 -0700

    feat: move away from create-react-app

---
+++ apps/web/webpack.config.js
@@ -0,0 +1,48 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const path = require("path");
+const webpack = require("webpack");
+
+const isDevelopment = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  mode: isDevelopment ? "development" : "production",
+  entry: "./src/index.tsx",
+  devServer: {
+    hot: true,
+  },
+  target: "web",
+  output: {
+    filename: "bundle.[hash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    isDevelopment && new webpack.HotModuleReplacementPlugin(),
+    isDevelopment && new ReactRefreshWebpackPlugin(),
+  ],
+  resolve: {
+    modules: [__dirname, "src", "node_modules"],
+    extensions: [".*", ".js", ".jsx", ".tsx", ".ts"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$|tsx/,
+        exclude: /node_modules/,
+        loader: require.resolve("babel-loader"),
+        options: {
+          plugins: [
+            isDevelopment && require.resolve("react-refresh/babel"),
+          ].filter(Boolean),
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
