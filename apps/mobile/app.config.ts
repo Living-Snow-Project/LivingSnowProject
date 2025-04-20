38bd5718837e47a6ae65f75b7eb65512e363bddd
Chris Chapin <cchapin@outlook.com>
2025-04-20 23:51:02 +0200

    fix: Android Google Maps Api Key needs to resolve from JS code

---
+++ apps/mobile/app.config.ts
@@ -0,0 +1,14 @@
+import { ConfigContext, ExpoConfig } from "@expo/config";
+
+export default ({ config }: ConfigContext): Partial<ExpoConfig> => {
+  // `config` is the object loaded from app.json. Here, we fill in secrets that
+  // we keep in the environment and out of code. For cloud builds, the secrets
+  // are stored in Expo and supplied via process.env. For local builds, the
+  // secrets can be stored in a .env file and loaded via direnv.
+
+  // we're overwriting fields that were previously defined in app.json, so we know they're non-null:
+  config.android!.config!.googleMaps!.apiKey =
+    process.env.ANDROID_GOOGLE_MAPS_API_KEY;
+
+  return config;
+};
