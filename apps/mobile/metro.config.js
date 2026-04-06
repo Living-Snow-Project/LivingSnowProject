b166f4e1dbd6f4f1c9dc2750eff7c69bbe08d778
Chris Chapin <cchapin@outlook.com>
2026-04-06 16:08:21 +0200

    upgrade: Expo 53

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -10,14 +10,14 @@ const workspaceRoot = path.resolve(projectRoot, "../..");
 const config = getDefaultConfig(projectRoot);
 
 // 1. Watch all files within the monorepo
-config.watchFolders = [workspaceRoot];
+//config.watchFolders = [workspaceRoot];
 // 2. Let Metro know where to resolve packages and in what order
-config.resolver.nodeModulesPaths = [
+/*config.resolver.nodeModulesPaths = [
   path.resolve(projectRoot, "node_modules"),
   path.resolve(workspaceRoot, "node_modules"),
-];
+];*/
 // 3. Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
-config.resolver.disableHierarchicalLookup = true;
+// config.resolver.disableHierarchicalLookup = true;
 // 4. keep mocks out of client bundle; previous blockList: /(website\\node_modules\\.*|.*\\__tests__\\.*)$/
 config.resolver.blockList =
   /(website\\node_modules\\.*|.*\\__tests__\\.*|website\\src\\mocks\\.*)$/;
